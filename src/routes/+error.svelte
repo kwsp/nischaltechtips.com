9bc79196e8700fa353d41ebf4cf50abb791c08c2
tnie <tnie@tuta.io>
2024-03-23 14:52:02 -0500

    custom 404 page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,19 @@
+<script>
+	import catputer from '$lib/images/cat_puter.gif';
+</script>
+
+<div class="notfound">
+	<p>
+		<i>Nothing to see here... go home :)</i>
+	</p>
+	<br />
+	<img src={catputer} alt="catputer" width="400" />
+</div>
+
+<style>
+	.notfound {
+		align-items: center;
+		justify-content: center;
+		text-align: center;
+	}
+</style>
