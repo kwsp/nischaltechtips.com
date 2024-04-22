c9884d3b1d8df9644c366ad421a89c3e18f98cb0
kwsp <tnie@tuta.io>
2024-04-22 23:37:59 +0000

    Deploying to gh-pages from @ kwsp/nischaltechtips.com@35ba35646ae675cf6fae2525129a80eda3f7d2a1 🚀

---
+++ _app/immutable/nodes/3.xWC5nWgc.js
@@ -0,0 +1,31 @@
+import{d as tt}from"../chunks/index.R8ovVqwX.js";import{s as et,p as at,n as nt}from"../chunks/scheduler.WFTk3rk3.js";import{S as rt,i as st,e as r,s as _,n as $,u as I,z as it,c as s,d as i,h as b,l as x,o as T,v as R,g as J,a as o,y as t,b as Z,w as B,k as V,t as D,x as F}from"../chunks/index.FDkmKbwm.js";import{N as G}from"../chunks/NTT.CTaVDdt8.js";const ot=tt,ht=!0,vt=Object.freeze(Object.defineProperty({__proto__:null,csr:ot,prerender:ht},Symbol.toStringTag,{value:"Module"})),lt=""+new URL("../assets/thankyou_for_choosing-NTT.SWmGuZ0x.pdf",import.meta.url).href,ct=""+new URL("../assets/prof_khanal.BJ9vndpb.webp",import.meta.url).href,ut=""+new URL("../assets/nischal_happi.Ur1kIVt2.webp",import.meta.url).href,dt=""+new URL("../assets/jinx_1.CaK-wkmX.webp",import.meta.url).href;function mt(Y){let h,N,a,w,q,u,L,d,O=`<span>Thank you for choosing Nischal Tech Tips!</span> <br/> <img src="${dt}" width="200" alt="Cat cat" loading="lazy"/>`,A,n,m,E,p,M,P,l,Q=`<img src="${ut}" width="250" alt="Nischal happi" loading="lazy" class="svelte-r754or"/>`,S,f,j,g,K,C,v,X,H;return u=new G({}),p=new G({}),g=new G({}),{c(){h=r("meta"),N=_(),a=r("div"),w=r("h1"),q=$("About "),I(u.$$.fragment),L=_(),d=r("a"),d.innerHTML=O,A=_(),n=r("div"),m=r("p"),E=$("Welcome to "),I(p.$$.fragment),M=$(`, the digital playground where Professor Nischal Khanal, a luminary in
+			optics and photonics, merges the precision of scientific research with the unabashed
+			enthusiasm of a true tech aficionado. Known in academic circles for his groundbreaking work on
+			lasers that could potentially rewrite the laws of physics, Nischal also harbors a secret
+			identity: a tech reviewer whose gadgets outnumber his research papers. With a mind that
+			operates at the speed of light, he dissects the latest technology, diving into the heart of
+			circuits and software with the zeal of a kid in a candy store. His ability to explain complex
+			phenomena in ways that even a toaster could understand (if toasters could understand human
+			speech, that is) makes him a revered figure in both the lab and the living room.`),P=_(),l=r("a"),l.innerHTML=Q,S=_(),f=r("p"),j=$("But "),I(g.$$.fragment),K=$(` isn't your average tech review site. Here, quantum mechanics meets quirky humor, and
+			product tests often go hand-in-hand with time-travel theories (because who hasn't wanted to fast-forward
+			to the release of the next big gadget?). Whether he's comparing camera resolutions to the resolving
+			power of the human eye or using photonics principles to optimize his gaming setup's lighting, Nischal
+			brings a fresh, hilarious perspective to the tech world. His reviews might tell you why your WiFi
+			signal is weaker than your coffee or how your smartwatch might be plotting to take over the world.
+			So, strap in and prepare for a journey where science fiction meets scientific facts, all delivered
+			with a hefty dose of laughter.`),C=_(),v=r("img"),this.h()},l(e){const k=it("svelte-plqohp",document.head);h=s(k,"META",{name:!0,content:!0}),k.forEach(i),N=b(e),a=s(e,"DIV",{class:!0});var c=x(a);w=s(c,"H1",{});var U=x(w);q=T(U,"About "),R(u.$$.fragment,U),U.forEach(i),L=b(c),d=s(c,"A",{href:!0,"data-svelte-h":!0}),J(d)!=="svelte-1pz8y0"&&(d.innerHTML=O),A=b(c),n=s(c,"DIV",{class:!0});var y=x(n);m=s(y,"P",{});var W=x(m);E=T(W,"Welcome to "),R(p.$$.fragment,W),M=T(W,`, the digital playground where Professor Nischal Khanal, a luminary in
+			optics and photonics, merges the precision of scientific research with the unabashed
+			enthusiasm of a true tech aficionado. Known in academic circles for his groundbreaking work on
+			lasers that could potentially rewrite the laws of physics, Nischal also harbors a secret
+			identity: a tech reviewer whose gadgets outnumber his research papers. With a mind that
+			operates at the speed of light, he dissects the latest technology, diving into the heart of
+			circuits and software with the zeal of a kid in a candy store. His ability to explain complex
+			phenomena in ways that even a toaster could understand (if toasters could understand human
+			speech, that is) makes him a revered figure in both the lab and the living room.`),W.forEach(i),P=b(y),l=s(y,"A",{href:!0,target:!0,"data-svelte-h":!0}),J(l)!=="svelte-kyqkrp"&&(l.innerHTML=Q),S=b(y),f=s(y,"P",{});var z=x(f);j=T(z,"But "),R(g.$$.fragment,z),K=T(z,` isn't your average tech review site. Here, quantum mechanics meets quirky humor, and
+			product tests often go hand-in-hand with time-travel theories (because who hasn't wanted to fast-forward
+			to the release of the next big gadget?). Whether he's comparing camera resolutions to the resolving
+			power of the human eye or using photonics principles to optimize his gaming setup's lighting, Nischal
+			brings a fresh, hilarious perspective to the tech world. His reviews might tell you why your WiFi
+			signal is weaker than your coffee or how your smartwatch might be plotting to take over the world.
+			So, strap in and prepare for a journey where science fiction meets scientific facts, all delivered
+			with a hefty dose of laughter.`),z.forEach(i),y.forEach(i),C=b(c),v=s(c,"IMG",{src:!0,alt:!0}),c.forEach(i),this.h()},h(){document.title="About",o(h,"name","description"),o(h,"content","About Nischal Tech Tips"),o(d,"href",lt),o(l,"href","https://www.youtube.com/watch?v=dQw4w9WgXcQ"),o(l,"target","_blank"),o(n,"class","text-wrap svelte-r754or"),at(v.src,X=ct)||o(v,"src",X),o(v,"alt","Prof. Khanal's invite to be a keynote speaker"),o(a,"class","text-column svelte-r754or")},m(e,k){t(document.head,h),Z(e,N,k),Z(e,a,k),t(a,w),t(w,q),B(u,w,null),t(a,L),t(a,d),t(a,A),t(a,n),t(n,m),t(m,E),B(p,m,null),t(m,M),t(n,P),t(n,l),t(n,S),t(n,f),t(f,j),B(g,f,null),t(f,K),t(a,C),t(a,v),H=!0},p:nt,i(e){H||(V(u.$$.fragment,e),V(p.$$.fragment,e),V(g.$$.fragment,e),H=!0)},o(e){D(u.$$.fragment,e),D(p.$$.fragment,e),D(g.$$.fragment,e),H=!1},d(e){e&&(i(N),i(a)),i(h),F(u),F(p),F(g)}}}class yt extends rt{constructor(h){super(),st(this,h,null,mt,et,{})}}export{yt as component,vt as universal};
